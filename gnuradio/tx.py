ddf9be65cb6651dcd83542d6a2caf8292ec4bb04
wentai <yewentai126@gmail.com>
2024-08-07 16:17:48 +0200

    PC

--- gnuradio/tx.py
+++ gnuradio/tx.py
@@ -6,17 +6,16 @@
 #
 # GNU Radio Python Flow Graph
 # Title: Not titled yet
-# Author: yewentai
-# GNU Radio version: 3.10.9.2
+# Author: root
+# GNU Radio version: 3.10.10.0
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from PyQt5 import QtCore
 from gnuradio import blocks
-import numpy
-from gnuradio import digital
-from gnuradio import gr
+import pmt
+from gnuradio import filter
 from gnuradio.filter import firdes
+from gnuradio import gr
 from gnuradio.fft import window
 import sys
 import signal
@@ -64,28 +63,17 @@ class tx(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 25000
-        self.encoder = encoder = digital.constellation_8psk().base()
-        self.encoder.set_npwr(1.0)
-        self.RF_Gain = RF_Gain = 10
-        self.Frequency = Frequency = 870e6
-        self.Bandwidth = Bandwidth = 20e6
+        self.samp_rate = samp_rate = 500000
+        self.center_freq = center_freq = 870e6
+        self.bandwidth = bandwidth = 250000
+        self.RF_gain = RF_gain = 50
 
         ##################################################
         # Blocks
         ##################################################
 
-        self._RF_Gain_range = qtgui.Range(0, 40, 1, 10, 200)
-        self._RF_Gain_win = qtgui.RangeWidget(self._RF_Gain_range, self.set_RF_Gain, "'RF_Gain'", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._RF_Gain_win)
-        self._Frequency_range = qtgui.Range(70e6, 6000e6, 1000, 870e6, 200)
-        self._Frequency_win = qtgui.RangeWidget(self._Frequency_range, self.set_Frequency, "'Frequency'", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._Frequency_win)
-        self._Bandwidth_range = qtgui.Range(200e3, 56e6, 100, 20e6, 200)
-        self._Bandwidth_win = qtgui.RangeWidget(self._Bandwidth_range, self.set_Bandwidth, "'Bandwidth'", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._Bandwidth_win)
         self.uhd_usrp_sink_0 = uhd.usrp_sink(
-            ",".join(('', '')),
+            ",".join(("serial=31DB53A", '')),
             uhd.stream_args(
                 cpu_format="fc32",
                 args='',
@@ -93,30 +81,23 @@ class tx(gr.top_block, Qt.QWidget):
             ),
             "",
         )
-        self.uhd_usrp_sink_0.set_samp_rate(Bandwidth)
-        # No synchronization enforced.
+        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_sink_0.set_time_now(uhd.time_spec(time.time()), uhd.ALL_MBOARDS)
 
-        self.uhd_usrp_sink_0.set_center_freq(Frequency, 0)
+        self.uhd_usrp_sink_0.set_center_freq(center_freq, 0)
         self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
-        self.uhd_usrp_sink_0.set_bandwidth(Bandwidth, 0)
-        self.uhd_usrp_sink_0.set_gain(RF_Gain, 0)
-        self.digital_constellation_modulator_0 = digital.generic_mod(
-            constellation=encoder,
-            differential=True,
-            samples_per_symbol=2,
-            pre_diff_code=True,
-            excess_bw=0.35,
-            verbose=False,
-            log=False,
-            truncate=False)
-        self.analog_random_source_x_0_0 = blocks.vector_source_b(list(map(int, numpy.random.randint(0, 8, 1000))), True)
+        self.uhd_usrp_sink_0.set_bandwidth(bandwidth, 0)
+        self.uhd_usrp_sink_0.set_gain(RF_gain, 0)
+        self.filter_fft_rrc_filter_0 = filter.fft_filter_ccc(1, firdes.root_raised_cosine(1, samp_rate, bandwidth, 0.35, 20), 1)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, 'C:\\Users\\Junchen Zhu\\Downloads\\Auto-Encoder-in-SIMO-System\\file\\tx.dat', True, 0, 0)
+        self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_random_source_x_0_0, 0), (self.digital_constellation_modulator_0, 0))
-        self.connect((self.digital_constellation_modulator_0, 0), (self.uhd_usrp_sink_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.filter_fft_rrc_filter_0, 0))
+        self.connect((self.filter_fft_rrc_filter_0, 0), (self.uhd_usrp_sink_0, 0))
 
 
     def closeEvent(self, event):
@@ -133,34 +114,30 @@ class tx(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+        self.filter_fft_rrc_filter_0.set_taps(firdes.root_raised_cosine(1, self.samp_rate, self.bandwidth, 0.35, 20))
+        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
 
-    def get_encoder(self):
-        return self.encoder
+    def get_center_freq(self):
+        return self.center_freq
 
-    def set_encoder(self, encoder):
-        self.encoder = encoder
+    def set_center_freq(self, center_freq):
+        self.center_freq = center_freq
+        self.uhd_usrp_sink_0.set_center_freq(self.center_freq, 0)
 
-    def get_RF_Gain(self):
-        return self.RF_Gain
+    def get_bandwidth(self):
+        return self.bandwidth
 
-    def set_RF_Gain(self, RF_Gain):
-        self.RF_Gain = RF_Gain
-        self.uhd_usrp_sink_0.set_gain(self.RF_Gain, 0)
+    def set_bandwidth(self, bandwidth):
+        self.bandwidth = bandwidth
+        self.filter_fft_rrc_filter_0.set_taps(firdes.root_raised_cosine(1, self.samp_rate, self.bandwidth, 0.35, 20))
+        self.uhd_usrp_sink_0.set_bandwidth(self.bandwidth, 0)
 
-    def get_Frequency(self):
-        return self.Frequency
+    def get_RF_gain(self):
+        return self.RF_gain
 
-    def set_Frequency(self, Frequency):
-        self.Frequency = Frequency
-        self.uhd_usrp_sink_0.set_center_freq(self.Frequency, 0)
-
-    def get_Bandwidth(self):
-        return self.Bandwidth
-
-    def set_Bandwidth(self, Bandwidth):
-        self.Bandwidth = Bandwidth
-        self.uhd_usrp_sink_0.set_samp_rate(self.Bandwidth)
-        self.uhd_usrp_sink_0.set_bandwidth(self.Bandwidth, 0)
+    def set_RF_gain(self, RF_gain):
+        self.RF_gain = RF_gain
+        self.uhd_usrp_sink_0.set_gain(self.RF_gain, 0)
 
 
 
---
+++ gnuradio/txrx.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Not titled yet
+# Author: Junchen Zhu
+# GNU Radio version: 3.10.10.0
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio import blocks
+import pmt
+from gnuradio import filter
+from gnuradio.filter import firdes
+from gnuradio import gr
+from gnuradio.fft import window
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import uhd
+import time
+
+
+
+class txrx(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Not titled yet")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "txrx")
+
+        try:
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 500000
+        self.center_freq = center_freq = 870e6
+        self.bandwidth = bandwidth = 250000
+        self.RF_gain = RF_gain = 50
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+            ",".join(("serial=31DB53A", '')),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='',
+                channels=list(range(0,1)),
+            ),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        # No synchronization enforced.
+
+        self.uhd_usrp_source_0.set_center_freq(center_freq, 0)
+        self.uhd_usrp_source_0.set_antenna("RX1", 0)
+        self.uhd_usrp_source_0.set_bandwidth(bandwidth, 0)
+        self.uhd_usrp_source_0.set_gain(RF_gain, 0)
+        self.uhd_usrp_sink_0 = uhd.usrp_sink(
+            ",".join(("serial=31DB53A", '')),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='',
+                channels=list(range(0,1)),
+            ),
+            "",
+        )
+        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
+        # No synchronization enforced.
+
+        self.uhd_usrp_sink_0.set_center_freq(center_freq, 0)
+        self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
+        self.uhd_usrp_sink_0.set_bandwidth(bandwidth, 0)
+        self.uhd_usrp_sink_0.set_gain(RF_gain, 0)
+        self.filter_fft_rrc_filter_0_0 = filter.fft_filter_ccc(1, firdes.root_raised_cosine(1, samp_rate, bandwidth, 0.35, 20), 1)
+        self.filter_fft_rrc_filter_0 = filter.fft_filter_ccc(1, firdes.root_raised_cosine(1, samp_rate, bandwidth, 0.35, 20), 1)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, 'C:\\Users\\Junchen Zhu\\Downloads\\Auto-Encoder-in-SIMO-System\\file\\tx.dat', True, 0, 0)
+        self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, 'C:\\Users\\Junchen Zhu\\Downloads\\Auto-Encoder-in-SIMO-System\\file\\rx.dat', False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_file_source_0, 0), (self.filter_fft_rrc_filter_0, 0))
+        self.connect((self.filter_fft_rrc_filter_0, 0), (self.uhd_usrp_sink_0, 0))
+        self.connect((self.filter_fft_rrc_filter_0_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.filter_fft_rrc_filter_0_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "txrx")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+        self.blocks_throttle2_0_0.set_sample_rate(self.samp_rate)
+        self.filter_fft_rrc_filter_0.set_taps(firdes.root_raised_cosine(1, self.samp_rate, self.bandwidth, 0.35, 20))
+        self.filter_fft_rrc_filter_0_0.set_taps(firdes.root_raised_cosine(1, self.samp_rate, self.bandwidth, 0.35, 20))
+        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+
+    def get_center_freq(self):
+        return self.center_freq
+
+    def set_center_freq(self, center_freq):
+        self.center_freq = center_freq
+        self.uhd_usrp_sink_0.set_center_freq(self.center_freq, 0)
+        self.uhd_usrp_source_0.set_center_freq(self.center_freq, 0)
+
+    def get_bandwidth(self):
+        return self.bandwidth
+
+    def set_bandwidth(self, bandwidth):
+        self.bandwidth = bandwidth
+        self.filter_fft_rrc_filter_0.set_taps(firdes.root_raised_cosine(1, self.samp_rate, self.bandwidth, 0.35, 20))
+        self.filter_fft_rrc_filter_0_0.set_taps(firdes.root_raised_cosine(1, self.samp_rate, self.bandwidth, 0.35, 20))
+        self.uhd_usrp_sink_0.set_bandwidth(self.bandwidth, 0)
+        self.uhd_usrp_source_0.set_bandwidth(self.bandwidth, 0)
+
+    def get_RF_gain(self):
+        return self.RF_gain
+
+    def set_RF_gain(self, RF_gain):
+        self.RF_gain = RF_gain
+        self.uhd_usrp_sink_0.set_gain(self.RF_gain, 0)
+        self.uhd_usrp_source_0.set_gain(self.RF_gain, 0)
+
+
+
+
+def main(top_block_cls=txrx, options=None):
+
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
